a35366475c9355f9dfe9d8fe35fa36e3353bbdb8
pikachu~ <71221586+AaronZhao99@users.noreply.github.com>
2021-08-31 21:19:35 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 > File Name:     skiplist.h
 > Author:        Aaron Zhao 
 > Created Time:  April 2021
-> Description:   
+> Description:   This is the execution file of the skip table project, which will perform functional tests such as the insertion, deletion, and printing of the skiplist
  ************************************************************************/
 
 #include <iostream>
@@ -15,12 +15,12 @@ int main() {
 	skipList.insert_element("1", "a"); 
 	skipList.insert_element("3", "b"); 
 	skipList.insert_element("7", "c"); 
-	skipList.insert_element("8", "sun"); 
-	skipList.insert_element("9", "xiu"); 
-	skipList.insert_element("19", "yang"); 
-	skipList.insert_element("19", "yang"); 
-	skipList.insert_element("19", "yang"); 
-	skipList.insert_element("19", "yanuuuuuuuuuuuuuuuuuug"); 
+	skipList.insert_element("8", "aaron"); 
+	skipList.insert_element("9", "aaron"); 
+	skipList.insert_element("19", "zhao"); 
+	skipList.insert_element("19", "zhao"); 
+	skipList.insert_element("19", "aaron zhao"); 
+	skipList.insert_element("19", "aaron zhao");
 
     std::cout << "skipList size:" << skipList.size() << std::endl;
 
